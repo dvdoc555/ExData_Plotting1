b9825cc81dee6f3cee209f0d4bb8a87f73043b2b
dvdoc555 <dvdoc555@gmail.com>
2016-06-19 09:23:56 -0400

    Add files via upload

---
+++ GraphingProjectFolder/plot2.R
@@ -0,0 +1,27 @@
+# download File if it doesn't exist
+
+if(!file.exists("household_power_consumption.txt")){
+  fileUrl<- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+  download.file(fileUrl,"Bugs.zip")
+  unzip("Bugs.zip")
+  unlink("Bugs.zip")
+}
+
+#read data
+hhdata<-read.table("household_power_consumption.txt", sep=";",header=TRUE,skip=66636,nrows=2280)
+hhnames<-read.table("household_power_consumption.txt", sep=";",nrows=1,header=TRUE)
+names(hhdata)<-names(hhnames)
+
+# Make character data into time data
+
+library(lubridate)
+hhdata$Date<-dmy(hhdata$Date)
+hhdata$Time<-hms(hhdata$Time)
+hhdata$dt<-hhdata$Date+hhdata$Time
+
+#plot Graph
+
+plot(hhdata$dt,hhdata$Global_active_power,ylab="Global Active Power(kilowatts)",type="l")
+
+dev.copy(png,file="plot2.png")
+dev.off()
